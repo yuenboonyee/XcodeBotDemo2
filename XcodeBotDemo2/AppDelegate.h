3c9c1fe06246164f0941f9dfa1f4de9fb40afa82
Yuen Boon Yee <yuen_boon_yee@sereneit.com>
2015-09-04 16:00:05 +1000

    Initial Commit

---
+++ XcodeBotDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XcodeBotDemo2
+//
+//  Created by Serene IT on 4/09/2015.
+//  Copyright (c) 2015 Serene IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XcodeBotDemo2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeBotDemo2
+//
+//  Created by Serene IT on 4/09/2015.
+//  Copyright (c) 2015 Serene IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
